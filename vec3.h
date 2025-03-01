62fce844db1bc818b676357db2a990b59e5af3ba
leonardo-toffalini <leonardotoffalini@gmail.com>
2025-03-01 21:03:31 +0100

    wip

--- vec3.h
+++ vec3.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "rt_common.h"
 #include <cmath>
 #include <iostream>
 #include <ostream>
@@ -45,6 +46,15 @@ public:
   }
 
   double length() const { return std::sqrt(length_squared()); }
+
+  static vec3 random() {
+    return vec3(random_double(), random_double(), random_double());
+  }
+
+  static vec3 random(double min, double max) {
+    return vec3(random_double(min, max), random_double(min, max),
+                random_double(min, max));
+  }
 };
 
 using point3 = vec3;
@@ -85,3 +95,22 @@ inline vec3 cross(const vec3 &u, const vec3 &v) {
 }
 
 inline vec3 unit_vector(const vec3 &v) { return v / v.length(); }
+
+inline vec3 random_unit_vector() {
+  while (true) {
+    vec3 p = vec3::random(-1, 1);
+    double lensq = p.length_squared();
+    if (1e-160 < lensq && lensq <= 1) {
+      return p / sqrt(lensq);
+    }
+  }
+}
+
+inline vec3 random_on_hemisphere(const vec3 &normal) {
+  vec3 rand_unit = random_unit_vector();
+  if (dot(rand_unit, normal) > 0.0) {
+    return rand_unit;
+  } else {
+    return -rand_unit;
+  }
+}
