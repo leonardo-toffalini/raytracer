5b8a4944697297946f31cdb24576225c1921d665
leonardo-toffalini <leonardotoffalini@gmail.com>
2025-03-02 16:32:04 +0100

    wip

--- camera.h
+++ camera.h
@@ -15,6 +15,8 @@ private:
   vec3 pixel_delta_u;
   vec3 pixel_delta_v;
   vec3 u, v, w;
+  vec3 defocus_disk_u;
+  vec3 defocus_disk_v;
 
   void initialize() {
     image_height = int(image_width / aspect_ratio);
@@ -24,10 +26,9 @@ private:
 
     center = lookfrom;
 
-    double focal_length = (lookfrom - lookat).length();
     double theta = degrees_to_radians(vfov);
     double h = std::tan(theta / 2);
-    double viewport_height = 2.0 * h * focal_length;
+    double viewport_height = 2.0 * h * focus_dist;
     double viewport_width =
         viewport_height * (double(image_width) / image_height);
 
@@ -42,16 +43,20 @@ private:
     pixel_delta_v = viewport_v / image_height;
 
     vec3 viewport_upper_left =
-        center - (focal_length * w) - viewport_u / 2 - viewport_v / 2;
+        center - (focus_dist * w) - viewport_u / 2 - viewport_v / 2;
     pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    double defocus_radius =
+        focus_dist * std::tan(degrees_to_radians(defocus_angle / 2.0));
+    defocus_disk_u = u * defocus_radius;
+    defocus_disk_v = v * defocus_radius;
   }
 
   ray get_ray(int i, int j) const {
-    // Generate a random ray around location i j
     vec3 offset = sample_square();
     vec3 pixel_sample = pixel00_loc + ((i + offset.x()) * pixel_delta_u) +
                         ((j + offset.y()) * pixel_delta_v);
-    vec3 ray_origin = center;
+    vec3 ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
     vec3 ray_direction = pixel_sample - ray_origin;
 
     return ray(ray_origin, ray_direction);
@@ -60,6 +65,11 @@ private:
   vec3 sample_square() const {
     // Return a vector of the form (U(-0.5, 0.5), U(-0.5, 0.5), 0)
     return vec3(random_double() - 0.5, random_double() - 0.5, 0);
+  }
+
+  point3 defocus_disk_sample() const {
+    point3 p = random_in_unit_disk();
+    return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
   }
 
   color ray_color(const ray &r, int depth, const hittable &world) const {
@@ -92,6 +102,9 @@ public:
   point3 lookfrom = point3(0, 0, 0);
   point3 lookat = point3(0, 0, -1);
   vec3 vup = vec3(0, 1, 0);
+
+  double defocus_angle = 0.0;
+  double focus_dist = 10.0;
 
   void render(const hittable &world) {
     initialize();
--- main.cc
+++ main.cc
@@ -22,7 +22,7 @@ int main(void) {
   auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
   auto material_left = make_shared<dielectric>(1.50);
   auto material_bubble = make_shared<dielectric>(1.00 / 1.50);
-  auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+  auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.2);
 
   world.add(
       make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
@@ -34,13 +34,17 @@ int main(void) {
   camera cam;
 
   cam.aspect_ratio = 16.0 / 9.0;
-  cam.image_width = 800;
-  cam.samples_per_pixel = 500;
+  cam.image_width = 400;
+  cam.samples_per_pixel = 100;
   cam.max_depth = 50;
-  cam.vfov = 35.0;
+
+  cam.vfov = 20.0;
   cam.lookfrom = point3(-2, 2, 1);
   cam.lookat = point3(0, 0, -1);
   cam.vup = point3(0, 1, 0);
+
+  cam.defocus_angle = 10.0;
+  cam.focus_dist = 3.4;
 
   cam.render(world);
 }
